7623f752a529e7deca74b67cc4c792d9ebf96a10
Ghanemla <ghanem.lamloumi1@gmail.com>
2023-06-28 11:44:01 +0200

    made window appear

--- Aim.py
+++ Aim.py
@@ -11,9 +11,13 @@ WIN =  pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Aim Trainer")
 
 def main():
-    run = true
+    run = True
     while run:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
                 break
+    pygame.QUIT()
+
+if __name__ == "__main__":
+    main()
