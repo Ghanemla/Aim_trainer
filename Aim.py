933e0c0701464a204db3def4c0f87a238a672355
Ghanemla <ghanem.lamloumi1@gmail.com>
2023-07-05 09:21:50 +0200

    target collision working

--- Aim.py
+++ Aim.py
@@ -44,6 +44,11 @@ class Target():
         pygame.draw.circle(win ,self.COLOR, (self.x, self.y), self.size * 0.6)
         pygame.draw.circle(win ,self.SECOND_COLOR, (self.x, self.y), self.size * 0.4)
 
+    def collide(self, x, y):
+        dis = math.sqrt((x - self.x)**2 + (y- self.y)**2)
+        return dis <= self.size
+    
+    
 def draw(win, targets):
      win.fill(BG_COLOR)
      for target in targets:
@@ -55,9 +60,16 @@ def main():
     targets = []
     clock = pygame.time.Clock()
 
+    target_pressed = 0
+    clicks = 0
+    misses = 0
+    start_time = time.time()
+
     pygame.time.set_timer(TARGET_EVENT,TARGET_INCREMENT)
     while run:
         clock.tick(60)
+        click = False
+        mouse_pos = pygame.mouse.get_pos()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
@@ -68,11 +80,21 @@ def main():
                 y = random.randint(TARGET_PADDING, HEIGHT - TARGET_PADDING)
                 target = Target(x,y)
                 targets.append(target)
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                click = True
+                clicks += 1
+
         for target in targets:
             target.update()
 
             if target.size <= 0:
                 targets.remove(target)
+                misses += 1
+
+            if click and target.collide(*mouse_pos):
+                targets.remove(target)
+                target_pressed += 1
 
         draw(WIN, targets)
     pygame.QUIT()
